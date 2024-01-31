380f8e46ba4d56acae6e0b5f8d9eda4d2567f21e
Aakif Nadeem <m.aakif.nadeem@gmail.com>
2024-01-31 12:38:16 +0500

    Initial Commit

---
+++ Structure SwiftUI/Structure_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Structure_SwiftUIApp.swift
+//  Structure SwiftUI
+//
+//  Created by Aakif Nadeem on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Structure_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
